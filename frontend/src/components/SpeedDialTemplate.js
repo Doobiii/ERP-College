af07b313dd6462174dd810b4f30ccfe9c7362080
Doobiii <Doobiii>
2024-04-19 01:53:11 +0530

    necessary changes

--- frontend/src/components/SpeedDialTemplate.js
+++ frontend/src/components/SpeedDialTemplate.js
@@ -1,34 +1,35 @@
-import React from 'react'
-import { SpeedDial, SpeedDialAction, styled } from '@mui/material';
-import TuneIcon from '@mui/icons-material/Tune';
+import React from "react";
+import { SpeedDial, SpeedDialAction, styled } from "@mui/material";
+import TuneIcon from "@mui/icons-material/Tune";
 
 const SpeedDialTemplate = ({ actions }) => {
-    return (
-        <CustomSpeedDial
-            ariaLabel="SpeedDial playground example"
-            icon={<TuneIcon />}
-            direction="left"
-        >
-            {actions.map((action) => (
-                <SpeedDialAction
-                    key={action.name}
-                    icon={action.icon}
-                    tooltipTitle={action.name}
-                    onClick={action.action}
-                />
-            ))}
-        </CustomSpeedDial>
-    )
-}
+  return (
+    <CustomSpeedDial
+      ariaLabel="SpeedDial playground example"
+      icon={<TuneIcon />}
+      direction="left"
+    >
+      {actions.map((action) => (
+        <SpeedDialAction
+          key={action.name}
+          icon={action.icon}
+          tooltipTitle={action.name}
+          onClick={action.action}
+        />
+      ))}
+    </CustomSpeedDial>
+  );
+};
 
-export default SpeedDialTemplate
+export default SpeedDialTemplate;
 
 const CustomSpeedDial = styled(SpeedDial)`
   .MuiSpeedDial-fab {
-    background-color: #032803;
-    
+    margin: 0.4rem;
+    background-color: #032303;
+
     &:hover {
-      background-color: green;
+      background-color: apple;
     }
   }
 `;
